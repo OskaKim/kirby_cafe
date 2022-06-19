5b09f50255cf958df28973dad05d72c611ffa622
OskaKim <o2kim0225@naver.com>
2022-06-19 15:07:27 +0900

    change line notify icon

--- line_notify.py
+++ line_notify.py
@@ -2,7 +2,7 @@ import requests
 import os;
 
 TARGET_URL = 'https://notify-api.line.me/api/notify'
-TOKEN = os.environ['LINE_ACCESS_TOKEN']
+TOKEN = 'NEeiey4ymAoPfEO199Dm1p4RKtPVtenHNxi7FQb39Bz'
 
 def line_notify(message):
     response = requests.post(
@@ -11,8 +11,12 @@ def line_notify(message):
         'Authorization': 'Bearer ' + TOKEN
     },
     data={
-        'message': message
-    }
+        "type": "text",
+        "message": message,
+        "imageThumbnail": "https://github.com/OskaKim/kirby_cafe/blob/main/kirby.jpg",
+    }   
     )
 
     print(response.text)
+
+line_notify("テスト")
