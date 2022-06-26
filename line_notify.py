a54ddceb15e362506efdd0404169ef26c39d6f8d
OskaKim <o2kim0225@naver.com>
2022-06-26 15:52:42 +0900

    delete unless code at line notify

--- line_notify.py
+++ line_notify.py
@@ -5,15 +5,13 @@ TARGET_URL = 'https://notify-api.line.me/api/notify'
 TOKEN = os.environ['LINE_ACCESS_TOKEN']
 
 def line_notify(message):
-    response = requests.post(
+    requests.post(
     TARGET_URL,
     headers={
         'Authorization': 'Bearer ' + TOKEN
     },
     data={
         "type": "text",
-        "message": message,
-        #"imageThumbnail": "https://raw.githubusercontent.com/OskaKim/kirby_cafe/main/kirby.jpg",
-        #"imageFullsize": "https://raw.githubusercontent.com/OskaKim/kirby_cafe/main/kirby.jpg",
+        "message": message
     }   
     )
